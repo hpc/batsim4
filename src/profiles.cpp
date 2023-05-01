806c2b3a91b7bd6138143c4d2edf133123e8ebfd
Craig Walker <cswalke1@coastal.edu>
2023-05-01 14:17:20 -0400

    namespace fs correction

--- src/profiles.cpp
+++ src/profiles.cpp
@@ -9,7 +9,7 @@
 #include <iostream>
 #if __has_include(<filesystem>)
 #include <filesystem>
-namespace fs = std::fileystem;
+namespace fs = std::filesystem;
 #elif __has_include(<experimental/filesystem>)
 #include <experimental/filesystem>
 namespace fs = std::experimental::filesystem;
